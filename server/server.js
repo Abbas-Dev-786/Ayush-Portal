0b2e93997e01524fd8527e99fb60b6863d2cf71d
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-09-30 16:10:49 +0530

    added server code

--- server/server.js
+++ server/server.js
@@ -1,8 +1,41 @@
 require("dotenv").config({ path: "./.env" });
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    process.env.DATABASE_URL.replace(
+      "<password>",
+      process.env.DATABASE_PASSWORD
+    ),
+    { autoIndex: true }
+  )
+  .then(() => console.log("DB Connected Successfull ✅"))
+  .catch((err) => console.log(err.message + "❌"));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Listening to request on port ${port}`);
+  console.log(`👂 Listening to request on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
+  });
 });
