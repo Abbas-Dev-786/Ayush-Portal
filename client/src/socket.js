a8bff8cb6547ef71235d7cbf8650a5d705de8ee7
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-10-20 19:21:49 +0530

    updated files

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.PROD ? "" : "http://localhost:8899";
+const URL = import.meta.env.PROD
+  ? "https://real-tunic-dove.cyclic.cloud"
+  : "http://localhost:8899";
 
 export const socket = io(URL, {
   autoConnect: false,
