83790e8a404690b37438d661a55eb02f0a058d7d
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-10-20 07:26:29 +0530

    bug fixes

--- client/src/components/dashboard/UserCard.jsx
+++ client/src/components/dashboard/UserCard.jsx
@@ -29,7 +29,7 @@ const UserCard = () => {
     >
       <CardMedia
         sx={{ height: 120, bgcolor: "gainsboro" }}
-        image="./../../../public/images/user_banner.png"
+        image="./images/user_banner.png"
         title="user image"
       />
       <CardContent
