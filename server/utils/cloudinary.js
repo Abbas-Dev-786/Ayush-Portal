ea1fdf993a970c353a3af6310531f26e55f6999e
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-11-01 22:01:10 +0530

    changes

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,28 +1,30 @@
-const cloudinary=require("cloudinary").v2
+const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
-})
+});
 
-const storage=new CloudinaryStorage({
-    cloudinary:cloudinary,
-    params:{
-      folder:"Profile_Images"
-    }
-})
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "Profile_Images",
+  },
+});
 
-const upload=multer({storage:storage,
+const upload = multer({
+  storage: storage,
   fileFilter: (req, file, cb) => {
-  let ext = path.extname(file.originalname);
-  if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-    cb(new Error("Unsupported file type!"), false);
-    return;
-  }
-  cb(null, true);
-},})
+    let ext = path.extname(file.originalname);
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+      cb(new Error("Unsupported file type!"), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
 
-module.exports={upload,storage}
+module.exports = { upload, storage };
